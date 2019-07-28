2cd79b1e6c17f392a4f731ff4c02e002a2f441a4
mirrormingzZ <1060996414@qq.com>
2019-07-28 11:30:34 +0800

    update img

--- hello-spring-cloud-alibaba-provider/src/main/java/cn/mirrorming/hello/spring/cloud/alibaba/provider/controller/ProviderController.java
+++ hello-spring-cloud-alibaba-provider/src/main/java/cn/mirrorming/hello/spring/cloud/alibaba/provider/controller/ProviderController.java
@@ -1,5 +1,6 @@
 package cn.mirrorming.hello.spring.cloud.alibaba.provider.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class ProviderController {
+
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping(value = "/echo/{message}")
     public String echo(@PathVariable String message) {
-        return "Hello Nacos Discovery " + message;
+        return "Hello Nacos Discovery " + message + " , From port :" + port;
     }
 }
