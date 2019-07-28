58ce2e463057b6ee1c088f6fe3c609639c0b226d
mirrormingzZ <1060996414@qq.com>
2019-07-28 10:52:41 +0800

    consumer

---
+++ hello-spring-cloud-alibaba-consumer/src/main/java/cn/mirrorming/spring/cloud/alibaba/consumer/ConsumerApplication.java
@@ -0,0 +1,13 @@
+package cn.mirrorming.spring.cloud.alibaba.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
---
+++ hello-spring-cloud-alibaba-consumer/src/main/java/cn/mirrorming/spring/cloud/alibaba/consumer/controller/ConsumerController.java
@@ -0,0 +1,30 @@
+package cn.mirrorming.spring.cloud.alibaba.consumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @GetMapping(value = "/echo/app/name")
+    public String echo() {
+        //使用 LoadBalanceClient 和 RestTemplate 结合的方式来访问
+        ServiceInstance serviceInstance = loadBalancerClient.choose("provider");
+        String url = String.format("http://%s:%s/echo/%s", serviceInstance.getHost(), serviceInstance.getPort(), appName);
+        return restTemplate.getForObject(url, String.class);
+    }
+}
