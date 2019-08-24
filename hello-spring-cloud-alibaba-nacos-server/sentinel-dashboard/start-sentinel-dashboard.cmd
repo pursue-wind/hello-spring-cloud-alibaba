dc8e1ad30222554cc742eda02430d74ba649acc0
mirrormingzZ <1060996414@qq.com>
2019-08-24 14:36:24 +0800

    升级skywalking 9.3

--- hello-spring-cloud-alibaba-nacos-server/sentinel-dashboard/start-sentinel-dashboard.cmd
+++ hello-spring-cloud-alibaba-nacos-server/sentinel-dashboard/start-sentinel-dashboard.cmd
@@ -1,1 +1,1 @@
-java -Dserver.port=8080 -Dcsp.sentinel.dashboard.server=localhost:8080 -Dproject.name=sentinel-dashboard -jar sentinel-dashboard.jar
+java -Dserver.port=8081 -Dcsp.sentinel.dashboard.server=localhost:8081 -Dproject.name=sentinel-dashboard -jar sentinel-dashboard.jar
